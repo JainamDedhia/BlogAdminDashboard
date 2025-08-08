29b9e43abf78d80b467979d17754ed6f32e0cea6
Jainam Dedhia <jainam.d@somaiya.edu>
2025-08-08 23:46:19 +0530

    updated the netlify

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+// vite.config.ts
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/', // ✅ VERY IMPORTANT for Netlify!
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
-});
+})
